d4d6a9667a7d53a882b5d6edab16c1abe25ed68a
Akshay BM <akshaybmnmd@gmail.com>
2020-11-21 22:42:17 +0530

    video added to extension

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  // port.postMessage("popup.js loaded");
   if (!localStorage.hasOwnProperty("permission")) {
     window.open("permission.html");
   }
@@ -8,7 +7,7 @@ $(document).ready(function () {
     name: "Sample Communication",
   });
 
-  port.postMessage("Hi BackGround");
+  // port.postMessage("Initiating this port");
 
   port.onMessage.addListener(function (msg) {
     console.log("message recieved" + msg);
@@ -17,6 +16,7 @@ $(document).ready(function () {
   document
     .getElementById("btn_register_client")
     .addEventListener("click", register_client);
+  console.log("added click event");
 
   document.getElementById("make_call").addEventListener("click", make_call);
 
@@ -29,19 +29,29 @@ $(document).ready(function () {
   }
 
   function make_call() {
-    var video = document.getElementById("videoElement");
+    var video1 = document.getElementById("videoElement1");
+    var video2 = document.getElementById("videoElement2");
     port.postMessage("video calling");
 
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
         .getUserMedia({ video: true })
         .then(function (stream) {
-          video.srcObject = stream;
+          video1.srcObject = stream;
+          video2.srcObject = stream;
         })
         .catch(function (err) {
           port.postMessage("Can't get userMedia: " + err);
         });
     }
+    window.setInterval(function () {
+      onAnswer();
+    }, 10000);
     document.getElementById("calling").style.display = "inline";
+  }
+
+  function onAnswer() {
+    document.getElementById("calling").style.display = "none";
+    document.getElementById("videoElement2").style.display = "inline";
   }
 });
